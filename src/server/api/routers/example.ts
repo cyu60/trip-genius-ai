97b772d3aa3a292becd55870e0586ce37cdab534
cyu60 <chinatchinat123@gmail.com>
2023-04-29 22:58:35 -0400

    add tailwind aspect-ratio

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -1,7 +1,38 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { Twilio } from "twilio";
 
+
+const sendTwilioMsg = async (number: string, info: string) => {
+  // const sendTwilioMsg = async (number: string, info: string) => {
+  // const accountSid = "AC5f16522c86a6a0b9f579e94beb3d700f";
+  // const authToken = "f07b77817a1c240d003500534bc84e95";
+  // const twilioNumber = "+18556437904";
+  const accountSid = process.env.TWILIO_ACC_SID;
+  const authToken = process.env.TWILIO_AUTH;
+  const twilioNumber = process.env.TWILIO_NUM;
+  // const myNumber = "+15712694598";
+  const myNumber = number;
+
+  if (accountSid && authToken && myNumber && twilioNumber) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+    const client= new Twilio(accountSid, authToken);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    await client.messages
+      .create({
+        from: twilioNumber,
+        to: myNumber,
+        body: info,
+      })
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      .then((message) => console.log(message.sid));
+  } else {
+    console.error(
+      "You are missing one of the variables you need to send a message"
+    );
+  }
+};
 export const exampleRouter = createTRPCRouter({
   hello: publicProcedure
     .input(z.object({ text: z.string() }))
@@ -9,6 +40,13 @@ export const exampleRouter = createTRPCRouter({
       return {
         greeting: `Hello ${input.text}`,
       };
+    }),
+  sendMessage: publicProcedure
+    .input(z.object({ textMessage: z.string() }))
+    .query(async ({ input }) => {
+      await sendTwilioMsg("+14437224218", input.textMessage);
+      // return grandparent;
+      return;
     }),
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.example.findMany();
